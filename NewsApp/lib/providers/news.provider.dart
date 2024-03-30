b3b5b537ec16824de180333b7286085bfe76d1af
Samuel Sihotang <samsihotang1@gmail.com>
2024-03-30 12:25:35 +0700

    news model api and provider

---
+++ NewsApp/lib/providers/news.provider.dart
@@ -0,0 +1,26 @@
+import 'package:NewsApp/helpers/api.dart';
+import 'package:NewsApp/models/topNews.model.dart';
+import 'package:NewsApp/utils/const.dart';
+import 'package:flutter/material.dart';
+
+class NewsProvider with ChangeNotifier {
+  bool isLoading = true;
+  TopNewsModel? resNews;
+  getTopNews() async {
+    final res = await api(
+        '${baseUrl}top-headlines?country=us&pageSize=100&apiKey=$API_KEY');
+
+    if (res.statusCode == 200) {
+      resNews = TopNewsModel.fromJson(res.data);
+    } else {
+      resNews = TopNewsModel();
+    }
+
+    isLoading = false;
+    notifyListeners();
+  }
+
+  TopNewsModel? getResNews() {
+    return resNews;
+  }
+}
