828bada60c61187371574b9c27236b2d7c9b7212
Samuel Sihotang <samsihotang1@gmail.com>
2024-03-30 10:21:08 +0700

    updated

--- NewsApp/lib/model/source_model.dart
+++ NewsApp/lib/model/source_model.dart
@@ -6,7 +6,7 @@ class Source {
   String name;
 
   //Let's create the constructor
-  Source({this.id, this.name});
+  Source({required this.id, required this.name});
 
   //Let's create the factory function to map the json
   factory Source.fromJson(Map<String, dynamic> json) {
